87e12108a74f0bf137de558acccc890a255a0ad7
wrightm-mac <wrightm@mac.com>
2017-03-19 17:40:30 +0100

    Dependency injection.

---
+++ LoginPicture/Source/Container.swift
@@ -0,0 +1,26 @@
+//
+//  Container.swift
+//  LoginPicture
+//
+//  Created by Michael Wright on 19/03/2017.
+//  Copyright © 2017 wrightm@mac.com. All rights reserved.
+//
+
+import Foundation
+
+
+open class Container {
+    
+//    // MARK:    Fields...
+//    
+//    private var types: [String: A] = [:]
+//    
+//    
+//    // MARK:    Methods...
+//    
+//    open func register<T>(forClass: Any, callback: @escaping () -> T) {
+//        let typeName = "\(type(of: forClass))"
+//        
+//        types[typeName] = callback
+//    }
+}
