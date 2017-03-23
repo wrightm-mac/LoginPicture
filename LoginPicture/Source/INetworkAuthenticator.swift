dfdb34ddf54fe837751aa9b08b25de0a82e1d67d
wrightm-mac <wrightm@mac.com>
2017-03-23 10:42:28 +0100

    Use dependency inject for network-caller.

---
+++ LoginPicture/Source/INetworkAuthenticator.swift
@@ -0,0 +1,9 @@
+//
+//  INetworkAuthenticator.swift
+//  LoginPicture
+//
+//  Created by Michael Wright on 23/03/2017.
+//  Copyright © 2017 wrightm@mac.com. All rights reserved.
+//
+
+import Foundation
---
+++ LoginPicture/Source/INetworkCaller.swift
@@ -0,0 +1,9 @@
+//
+//  INetworkCaller.swift
+//  LoginPicture
+//
+//  Created by Michael Wright on 23/03/2017.
+//  Copyright © 2017 wrightm@mac.com. All rights reserved.
+//
+
+import Foundation
